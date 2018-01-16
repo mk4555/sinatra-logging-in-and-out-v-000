99f69647b30b3f592756bd0033e83752555dd78c
Min Kim <mk4555@nyu.edu>
2018-01-16 23:07:21 +0000

    automatically backed up by learn

--- db/migrate/20180116230609_users.rb
+++ db/migrate/20180116230609_users.rb
@@ -1,7 +1,7 @@
 class Users < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :username
       t.string :password
       t.string :balance
     end
