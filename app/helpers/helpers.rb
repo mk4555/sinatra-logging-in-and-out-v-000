11140e8973555469452f2946125afe4c2742207a
Min Kim <mk4555@nyu.edu>
2018-01-16 23:54:00 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,10 @@
 class Helpers
   def current_user(session)
-    user = User.find_by_id(session[:id])
+    user = User.find_by_id(session[:user_id])
     user
   end
 
   def is_logged_in?
+
   end
 end
